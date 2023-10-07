e5cfb22f408a45d6729464f2d0f0e7d0cd3c3f68
UselessNode <78096367+UselessNode@users.noreply.github.com>
2023-10-08 00:57:34 +0700

    Добавьте файлы проекта.

---
+++ Controllers/CallbackController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Utils;
+
+namespace TimeLecture.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        /// <summary>
+        /// Конфигурация приложения
+        /// </summary>
+
+
+        private readonly IConfiguration _configuration;
+
+        private readonly IVkApi _vkApi;
+
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
+        {
+            _vkApi = vkApi;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Callback([FromBody] Updates updates)
+        {
+            // Проверяем, что находится в поле "type" 
+            switch (updates.Type)
+            {
+                // Если это уведомление для подтверждения адреса
+                case "confirmation":
+                    // Отправляем строку для подтверждения 
+                    return Ok(_configuration["Config:Confirmation"]);
+
+
+                // Новое сообщение
+                case "message_new":
+                    {
+                        //var msg = new VkResponse(updates.Object);
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            //PeerId = msg.PeerId.Value,
+                            Message = "你好, 哎呀"
+                        });
+                        break;
+                    }
+            }
+            // Возвращаем "ok" серверу Callback API
+            return Ok("ok");
+        }
+    }
+}
